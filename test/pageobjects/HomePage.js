b5742e9447483c162c900cd71c3cfa3952b7646f
Romapsp <surkov.ron@gmail.com>
2023-03-27 23:15:25 +0200

    added all files

---
+++ test/pageobjects/HomePage.js
@@ -0,0 +1,230 @@
+
+
+class HomePage {
+    get signInLink() {
+        return $('#gh-ug > a')
+    }
+
+    get registerLink() {
+        return $('#gh-ug-flex > a')
+    }
+
+    get dailyDealsLink() {
+        return $('#gh-p-1 > a')
+    }
+
+    get brandOutletLink() {
+        return $('#gh-p-4 > a')
+    }
+
+    get helpAndContactLink() {
+        return $('#gh-p-3 > a')
+    }
+
+    get sellLink() {
+        return $('#gh-p-2 > a')
+    }
+
+    get watchlistLink() {
+        return $('#gh-wl-click > div > a.gh-eb-li-a.gh-rvi-menu.watchlist-menu')
+    }
+
+    get expandedWatchlist() {
+        return $('#gh-wl-click > div > a.gh-acc-exp-a.gh-acc-a2.gh-control')
+    }
+
+    get expandedWatchlistModule() {
+        return $('#gh-wl-click-flyout')
+    }
+
+    get myEbayBtn() {
+        return $('#gh-eb-My > div > a.gh-eb-li-a.gh-rvi-menu')
+    }
+
+    get myEbayMenuList() {
+        return $('#gh-ul-nav').$$('li a')
+    }
+
+    get notificationBtn() {
+        return $('#gh-eb-Alerts > div > button')
+    }
+
+    get notificationMenuContent() {
+        return $('#ghn-err > span > a')
+    }
+
+    get cartBtn() {
+        return $('#gh-minicart-hover > div > a.gh-eb-li-a.gh-rvi-menu')
+    }
+
+    get cartMenuContent() {
+        return $('#gh-minicart-hover-body > div:nth-child(3) > div > div')
+    }
+
+    get ebayLogoBtn() {
+        return $('#gh-la')
+    }
+
+    get searchInput() {
+        return $('#gh-ac')
+    }
+
+    get searchBtn() {
+        return $('#gh-btn')
+    }
+
+    get allCategoriesDropdown() {
+        return $('#gh-cat-box')
+    }
+
+    get allCategoriesList() {
+        return $('#gh-cat').$$('option')
+    }
+
+    get advancedLink() {
+        return $('#gh-as-a')
+    }
+
+    get savedLink() {
+        return $('#mainContent > div.hl-cat-nav > ul > li.saved > a')
+    }
+
+    get motorsMenuBtn() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(3) > a')
+    }
+
+    get motorsMenuPartAndAccessoriesLinks() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(3) > div.hl-cat-nav__flyout > div.hl-cat-nav__sub-cats > nav:nth-child(1)').$$('li a')
+    }
+
+    get motorsMenuVehiclesLinks() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(3) > div.hl-cat-nav__flyout > div.hl-cat-nav__sub-cats > nav:nth-child(2) > ul').$$('li a')
+    }
+
+    get motorsImage() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(3) > div.hl-cat-nav__flyout > div.hl-cat-nav__rtm > div > map > area')
+    }
+
+    get electronicsMenuBtn() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > a')
+    }
+
+    get electronicsMenuTopCategoriesLinks() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > div.hl-cat-nav__flyout > div.hl-cat-nav__sub-cats > nav:nth-child(1) > ul').$$('li a')
+    }
+
+    get ElectronicsImage() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > div.hl-cat-nav__flyout > div.hl-cat-nav__rtm > div > map > area')
+    }
+
+    get electronicsMenuPopularCategoriesLinks() {
+        return $('#mainContent > div.hl-cat-nav > ul > li:nth-child(4) > div.hl-cat-nav__flyout > div.hl-cat-nav__sub-cats > nav:nth-child(2) > ul').$$('li a')
+    }
+
+    async clickOnSignInLink() {
+        this.signInLink.click()
+    }
+
+    async clickOnRegisterLink() {
+        this.registerLink.click()
+    }
+
+    async clickOnDailyDealsLink() {
+        this.dailyDealsLink.click()
+    }
+
+    async clickOnBrandOutletLink() {
+        this.brandOutletLink.click()
+    }
+
+    async clickOnHelpAndContactLink() {
+        this.helpAndContactLink.click()
+    }
+
+    async clickOnSellLink() {
+        this.sellLink.click()
+    }
+
+    async clickOnWatchlistLink() {
+        this.watchlistLink.click()
+    }
+
+    async expandedWatchlistValue() {
+        await this.expandedWatchlistModule.waitForDisplayed()
+        return this.expandedWatchlist.getAttribute('aria-expanded')
+    }
+
+    async hoverOnMyEbayBtn() {
+        await this.myEbayBtn.moveTo();
+      }
+
+    async allMyEbayMenuLinksAreDisplayed() {
+        const links = await this.myEbayMenuList;
+        for (let i = 0; i < links.length; i++) {
+          if (!(await links[i].isDisplayed())) {
+            return false
+          }
+        }
+        return true
+      }
+      
+    async myEbayMenuListLength() {
+        return (await this.myEbayMenuList).length
+      }
+
+    async hoverOnNotificationBtn() {
+        await this.notificationBtn.moveTo()
+    }
+
+    async hoverOnCartBtn() {
+        await this.cartBtn.moveTo()
+    }
+
+    async searchValue(value) {
+        await this.searchInput.setValue(value)
+        await this.searchBtn.click()
+    }
+
+    async openAllCategoriesList() {
+        await this.allCategoriesDropdown.click()
+    }
+
+    async openAdvancedLink() {
+        await this.advancedLink.click()
+    }
+
+    async openSavedLink() {
+        await this.savedLink.click()
+    }
+
+    async hoverOnMotorsMenuBtn() {
+        await this.motorsMenuBtn.moveTo();
+        await browser.pause(500)
+    }
+    
+    async openMotorLink() {
+        await this.motorsMenuVehiclesLinks[6].click()
+    }
+
+    async clickOnMotorsImage() {
+        await this.motorsImage.waitForExist()
+        await this.motorsImage.click()
+    }
+
+    async hoverOnElectronicsMenuBtn() {
+        await this.electronicsMenuBtn.moveTo()
+        await browser.pause(500)
+    }
+
+    async openElectronicsLink() {
+        await this.electronicsMenuTopCategoriesLinks[7].click()
+    }
+
+    async clickOnElectronicsImage() {
+        await this.ElectronicsImage.waitForExist()
+        await this.ElectronicsImage.click()
+    }
+}
+
+export default new HomePage()
+
---
+++ test/specs/HomePageTests.js
@@ -0,0 +1,152 @@
+import HomePage from '../pageobjects/HomePage.js'
+
+
+
+describe('Ebay HomePage', () => {
+    beforeEach(async () => {
+        await browser.url('https://www.ebay.com/')
+      })
+    it.skip ('displays title "Electronics, Cars, Fashion, Collectibles & More | eBay"', async() => {
+        await expect(browser).toHaveTitle('Electronics, Cars, Fashion, Collectibles & More | eBay') 
+    })
+    it.skip ('displays and opens "Sign in" link', async() => {
+        await expect(HomePage.signInLink).toBeDisplayed()
+        await HomePage.clickOnSignInLink()
+        await expect(browser).toHaveTitle('Security Measure')
+        await browser.url('https://www.ebay.com/')
+    })
+    it.skip ('displays and opens "register" link', async() => {
+        await expect(HomePage.registerLink).toBeDisplayed()
+        await HomePage.clickOnRegisterLink()
+        await expect(browser).toHaveTitle('Security Measure')
+    })
+    it.skip ('displays and opens "Daily Deals" link', async() => {
+        await expect(HomePage.dailyDealsLink).toBeDisplayed()
+        await HomePage.clickOnDailyDealsLink()
+        await expect(browser).toHaveTitle('Daily Deals on eBay | Best deals and Free Shipping')
+    })
+    it.skip ('displays and opens "Brand Outlet" link', async() => {
+        await expect(HomePage.brandOutletLink).toBeDisplayed()
+        await HomePage.clickOnBrandOutletLink()
+        await expect(browser).toHaveTitle('Brand Outlet products for sale | eBay')
+    })
+    it.skip ('displays and opens "Help & Contact" link', async() => {
+        await expect(HomePage.helpAndContactLink).toBeDisplayed()
+        await HomePage.clickOnHelpAndContactLink()
+        await expect(browser).toHaveTitle('Security Measure')
+    })
+    it.skip ('displays and opens "Sell" Link', async() => {
+        await expect(HomePage.sellLink).toBeDisplayed()
+        await HomePage.clickOnSellLink()
+        await expect(browser).toHaveTitle('Selling on eBay | Electronics, Fashion, Home & Garden | eBay')
+    })
+    it.skip ('displays and opens "Watchlist" Link', async() => {
+        await expect(HomePage.watchlistLink).toBeDisplayed()
+        await HomePage.clickOnWatchlistLink()
+        await expect(await HomePage.expandedWatchlistValue()).toBe('true')
+    })
+    it.skip ('displays and opens "My Ebay" menu on hover action', async() => {
+        await expect(HomePage.myEbayBtn).toBeDisplayed()
+        await HomePage.hoverOnMyEbayBtn()
+        await expect(HomePage.myEbayMenuList).toBeElementsArrayOfSize(13);
+        await expect(HomePage.allMyEbayMenuLinkAreDisplayed()).toBeTruthy();
+    })
+    it.skip ('displays and opens "Notification" menu on hover action', async() => {
+        await expect(HomePage.notificationBtn).toBeDisplayed()
+        await HomePage.hoverOnNotificationBtn()
+        console.log(`the text is ${await HomePage.notificationMenuContent.getText()}`)
+        await expect(HomePage.notificationMenuContent).toHaveText(/Please sign-in to view notifications./)
+    })
+    it.skip ('displays and opens "Cart" menu on hover action', async() => {
+        await expect(HomePage.cartBtn).toBeDisplayed()
+        await HomePage.hoverOnCartBtn()
+        console.log(`the text is ${await HomePage.cartMenuContent.getText()}`)
+        await expect(HomePage.cartMenuContent).toHaveText(/Your cart is empty\nTime to start shopping!/)
+    })
+    it.skip ('displays and refreshed the page by clicking on "Ebay" logo button', async() => {
+        await expect(HomePage.ebayLogoBtn).toBeDisplayed()
+        await HomePage.ebayLogoBtn.click()
+        await expect(browser).toHaveUrl('https://www.ebay.com/')
+    })
+    it.skip ('displays and searches for the item', async() => {
+        await expect(HomePage.searchInput).toBeDisplayed()
+        // You can type any value instead of "macbook", but don't forget to change the value in last expect as well
+        await HomePage.searchValue('macbook')
+        await expect(browser).toHaveUrlContaining('https://www.ebay.com/sch/')
+        await expect(browser).toHaveUrlContaining('macbook')
+    })
+    it.skip ('displays and opens "All categories" dropdown', async() => {
+        await expect(HomePage.allCategoriesDropdown).toBeDisplayed()
+        await HomePage.openAllCategoriesList()
+        await expect(HomePage.allCategoriesList).toBeElementsArrayOfSize({gte: 35})
+    }) 
+    it.skip ('displays and opens "Advanced" search page', async() => {
+        await expect(HomePage.advancedLink).toBeDisplayed()
+        await HomePage.openAdvancedLink()
+        await expect(browser).toHaveUrl('https://www.ebay.com/sch/ebayadvsearch')
+    })
+    it.skip ('displays and opens "Saved" page', async() => {
+        await expect(HomePage.savedLink).toBeDisplayed()
+        await HomePage.openSavedLink()
+        await expect(browser).toHaveTitle('Security Measure')
+    })
+    it.skip ('displays 7 links in "Parts & Accessories" in Motors Menu', async() => {
+        await expect(HomePage.motorsMenuBtn).toBeDisplayed()
+        await HomePage.hoverOnMotorsMenuBtn()
+        const expectedMotorsLinks = ['All Parts & Accessories', 'Car & Truck Parts', 'Motorcycle Parts', 'Automotive Tools & Supplies', 'Apparel & Merchandise', 'Motors Deals', 'My Garage']
+        const actualMotorsLinks = await HomePage.motorsMenuPartAndAccessoriesLinks.map((motorsMenuBtn) => motorsMenuBtn.getText())
+        console.log("Expected links: ", expectedMotorsLinks)
+        console.log("Actual links: ", actualMotorsLinks)
+        await expect(expectedMotorsLinks).toEqual(actualMotorsLinks)
+    })
+    it.skip ('displays 7 links in "Vehicles" in Motors Menu', async() => {
+        await expect(HomePage.motorsMenuBtn).toBeDisplayed()
+        await HomePage.hoverOnMotorsMenuBtn()
+        const expectedMotorsLinks = ['Cars & Trucks', 'Classics', 'Motorcycles', 'Powersports', 'RVs & Campers', 'Trailers & Other Vehicles', 'Boats']
+        const actualMotorsLinks = await HomePage.motorsMenuVehiclesLinks.map((motorsMenuBtn) => motorsMenuBtn.getText())
+        console.log("Expected links: ", expectedMotorsLinks)
+        console.log("Actual links: ", actualMotorsLinks)
+        await expect(expectedMotorsLinks).toEqual(actualMotorsLinks)
+    })
+    it.skip ('opens a link from Motors Menu', async() => {
+        await HomePage.hoverOnMotorsMenuBtn()   
+        await HomePage.openMotorLink()
+        await expect(browser).toHaveUrlContaining('Boats')
+    })
+    it.skip ('displays and opens image in Motors Menu', async() => {
+        await HomePage.hoverOnMotorsMenuBtn()   
+        await HomePage.clickOnMotorsImage()
+        await expect(browser).toHaveUrlContaining('Auto-Parts-and-Vehicles')
+    })
+    it.skip ('displays 8 links in "Top Categories" in Electronics Menu', async() => {
+        await expect(HomePage.electronicsMenuBtn).toBeDisplayed()
+        await HomePage.hoverOnElectronicsMenuBtn()
+        const expectedElectronicsLinks = ['Computers, Tablets & Network Hardware', 'Cameras & Photo', 'Cell Phones & Smartphones', 'Cell Phone Cases, Covers & Skins', 'TV, Video & Home Audio Electronics', 'Vehicle Electronics & GPS', 'Headphones', 'Surveillance & Smart Home Electronics']
+        const actualElectronicsLinks = await HomePage.electronicsMenuTopCategoriesLinks.map((electronicsMenuBtn) => electronicsMenuBtn.getText())
+        console.log("Expected links: ", expectedElectronicsLinks)
+        console.log("Actual links: ", actualElectronicsLinks)
+        await expect(expectedElectronicsLinks).toEqual(actualElectronicsLinks)
+    })
+    it.skip ('displays 8 links in "Popular Categories" in Electronics Menu', async() => {
+        await expect(HomePage.electronicsMenuBtn).toBeDisplayed()
+        await HomePage.hoverOnElectronicsMenuBtn()
+        const expectedElectronicsLinks = ['eBay Refurbished', 'Video Games', 'Nintendo Toys to Life', 'Apple iPhone', 'PC Desktops & All-In-One Computers', 'Computer Graphics Cards', 'Tablets & eReaders', 'Laptops & Netbooks']
+        const actualElectronicsLinks = await HomePage.electronicsMenuPopularCategoriesLinks.map((electronicsMenuBtn) => electronicsMenuBtn.getText())
+        console.log("Expected links: ", expectedElectronicsLinks)
+        console.log("Actual links: ", actualElectronicsLinks)
+        await expect(expectedElectronicsLinks).toEqual(actualElectronicsLinks)
+    })
+    it.skip ('opens a link from Electronics Menu', async() => {
+        await HomePage.hoverOnElectronicsMenuBtn()   
+        await HomePage.openElectronicsLink()
+        await expect(browser).toHaveUrlContaining('Surveillance-Smart-Home-Devices')
+    })
+    it.skip ('displays and opens image in Electronics Menu', async() => {
+        await HomePage.hoverOnElectronicsMenuBtn()   
+        await HomePage.clickOnElectronicsImage()
+        await expect(browser).toHaveUrlContaining('Electronics')
+    })
+    
+}) 
+
+
